909d3ee4477522922becf101265613add5c40977
Phong200200 <quocphong20022000@gmail.com>
2021-08-23 20:40:38 +0700

    update permission

--- app/src/main/java/com/example/contentproviderwithroom/Model/Menu.java
+++ app/src/main/java/com/example/contentproviderwithroom/Model/Menu.java
@@ -9,30 +9,19 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = Menu.TABLE_NAME)
 public class Menu {
-    /** The name of the Menu table. */
     public static final String TABLE_NAME = "menus";
 
-    /** The name of the ID column. */
     public static final String COLUMN_ID = BaseColumns._ID;
 
-    /** The name of the name column. */
     public static final String COLUMN_NAME = "name";
 
-    /** The unique ID of the menu. */
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(index = true, name = COLUMN_ID)
     public long id;
 
-    /** The name of the menu. */
     @ColumnInfo(name = COLUMN_NAME)
     public String name;
 
-    /**
-     * Create a new {@link Menu} from the specified {@link ContentValues}.
-     *
-     * @param values A {@link ContentValues} that at least contain {@link #COLUMN_NAME}.
-     * @return A newly created {@link Menu} instance.
-     */
     public static Menu fromContentValues(ContentValues values) {
         final Menu menu = new Menu();
         if (values.containsKey(COLUMN_ID)) {
